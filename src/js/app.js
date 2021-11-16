5112b645522dc14256552de1a7e3fed989ac34fa
Kristiqn95 <55980434+Kristiqn95@users.noreply.github.com>
2021-11-16 16:08:55 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  
-  const products = document.getElementByClassName('product');
-  
-  for(let element of products) {
+
+  const products = document.getElementsByClassName('product');
+
+  for (let element of products) {
     element.dataset.price = element.getElementsByClassName('price')[0].innerText;
   }
 });
